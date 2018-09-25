ef9da66732ef6f750f88583bcfbcda6ce1b72257
Alfredwooden <Alfredwooden>
2018-09-25 12:59:25 -0300

    Finished section 2

--- Project-2/app.ts
+++ Project-2/app.ts
@@ -1,8 +1,6 @@
-console.log("It works!");
-
 // string
 let myName: string = 'Alfred';
-console.log('My name: ' + myName);
+console.log('String = My name: ' + myName);
 
 // number
 let myAge: number = 25;
@@ -15,7 +13,7 @@ let myRealAge: number;
 myRealAge = 25;
 
 let hobbies: string[] = ['Cooking', 'Sports'];
-console.log('Hobbies: ' + hobbies[0]);
+console.log('Assign types = Hobbies: ' + hobbies[0]);
 // hobbies = ['A string'];
 
 // Tuples
@@ -30,24 +28,24 @@ enum Color {
 }
 
 let myColor: Color = Color.Red;
-console.log('My color: ' + myColor)
+console.log('Enum = My color: ' + myColor);
 
 // Any
 let car: any = 'BMW';
-console.log('My car: ' + car);
+console.log('Type ANY = My car: ' + car);
 car = { brand: 'BMW', series: 3};
-console.log('Brand + Series: ' + JSON.stringify(car));
+console.log('Type ANY = Brand + Series: ' + JSON.stringify(car));
 
 // Functions
 function returnMyName(): string {
     return myName;
 }
 
-console.log('My name through a function: ' + returnMyName());
+console.log('Functions = My name through a function: ' + returnMyName());
 
 // Void
 function sayHello(): void {
-    console.log('Hello!');
+    console.log('Void = Hello!');
     // return myAge; ----- Cant return anything with type 'void'
 }
 
@@ -55,12 +53,12 @@ function sayHello(): void {
 function multiply(value1: number, value2: number): number {
     return value1 * value2;
 }
-console.log('Value 1 + Value 2: ' + multiply(2, 5));
+console.log('Argument types = Value 1 + Value 2: ' + multiply(2, 5));
 
 // Function types
 let myMultiply: (val1: number, val2: number) => number;
 myMultiply = multiply;
-console.log(myMultiply(10, 2));
+console.log('Function types = ' + myMultiply(10, 2));
 
 // Objects
 let userData: { name: string, age: number } = {
@@ -76,3 +74,40 @@ let complex: {data: number[], output: (all: boolean) => number[]} = {
         return this.data;
     }
 }
+
+// complex = {};
+
+// Type alias
+type Complex = {data: number[], output: (all: boolean) => number[]};
+
+let complex2: Complex = {
+    data: [100, 3.99, 10],
+
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+}
+
+// Union types
+let myRealRealAge: number | string = 25;
+myRealRealAge = '25';
+
+// Check types
+let finalValue: number = 3;
+if(typeof finalValue == 'number'){
+    console.log('Check types = finalValue is a number')
+}
+
+// Never
+
+function neverReturns(): never {
+    throw new Error('An error!');
+}
+
+// Nullable types
+let canBeNull: number | null = 12; 
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+let canThisBeAny = null;
+canThisBeAny = 12;
